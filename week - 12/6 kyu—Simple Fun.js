b179fecc4d50732bb242b1040277d0365974778e
rjwx60 <614054722@qq.com>
2019-06-24 07:06:01 +0800

    d54

---
+++ week - 12/6 kyu—Simple Fun.js
@@ -0,0 +1,34 @@
+/**
+ * Source:  
+ * https://www.codewars.com/kata/draw-the-stereoscopic-graphics-of-building-blocks
+ * https://www.codewars.com/kata/5bdb9fa44a8a178df80000ee/train/javascript
+ * 
+ * Author:  
+ * 
+ * Title:
+ * 
+ * Example: 
+ * 
+ * Translate:
+ */
+
+
+/** 
+ * My solution - ms
+ */
+
+
+/** 
+ * Other solution:
+ * Source: https://www.codewars.com/kata/regex-validate-pin-code/solutions
+ * 
+ * Copyright belongs to the original author. 
+ * If it infringes on your rights and interests, please email me: rjwx60@gmail.com
+ */
+
+ 
+/**
+ * Think:
+ */
+
+
